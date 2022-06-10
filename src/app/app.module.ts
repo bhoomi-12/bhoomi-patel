7272a31f32415a8722b422ae68051279a4efac98
bhoomi-12 <bhoomo2711@gmail.com>
2022-06-10 13:43:34 +0530

    first-app-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavvComponent } from './navv/navv.component';
+import { FooterComponent } from './footer/footer.component';
+import { ClumsyComponent } from './clumsy/clumsy.component';
+import { ErrorComponent } from './error/error.component';
+import { BhoomiComponent } from './bhoomi/bhoomi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavvComponent,
+    BhoomiComponent,
+    FooterComponent,
+    ClumsyComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
